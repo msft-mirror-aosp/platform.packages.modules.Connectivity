ff3a00633830292b49e83d56efc803d63ccc6025
Sandro Montanari <sandrom@google.com>
2024-12-17 12:00:25 +0000

    Lazy initialize CertificateTransparencyDownloader and DataStore
    To prevent Post Boot unlock regressions.
    
    Flag: android.security.certificate_transparency_configuration
    Bug: 383437444
    Test: atest NetworkSecurityUnitTests
    Change-Id: I9da001863998aba7cdf962275d8611e32e8efa84

--- networksecurity/service/src/com/android/server/net/ct/CertificateTransparencyJob.java
+++ networksecurity/service/src/com/android/server/net/ct/CertificateTransparencyJob.java
@@ -39,6 +39,8 @@ public class CertificateTransparencyJob extends BroadcastReceiver {
     private final CertificateTransparencyDownloader mCertificateTransparencyDownloader;
     private final AlarmManager mAlarmManager;
 
+    private boolean mDependenciesReady = false;
+
     /** Creates a new {@link CertificateTransparencyJob} object. */
     public CertificateTransparencyJob(
             Context context,
@@ -51,9 +53,6 @@ public class CertificateTransparencyJob extends BroadcastReceiver {
     }
 
     void initialize() {
-        mDataStore.load();
-        mCertificateTransparencyDownloader.initialize();
-
         mContext.registerReceiver(
                 this, new IntentFilter(ACTION_JOB_START), Context.RECEIVER_EXPORTED);
         mAlarmManager.setInexactRepeating(
@@ -76,6 +75,11 @@ public class CertificateTransparencyJob extends BroadcastReceiver {
         }
         if (Config.DEBUG) {
             Log.d(TAG, "Starting CT daily job.");
+        }
+        if (!mDependenciesReady) {
+            mDataStore.load();
+            mCertificateTransparencyDownloader.initialize();
+            mDependenciesReady = true;
         }
 
         mDataStore.setProperty(Config.CONTENT_URL, Config.URL_LOG_LIST);
