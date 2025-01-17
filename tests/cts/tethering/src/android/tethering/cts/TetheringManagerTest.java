c7ed1a195a209d15378033030c29d58cf79dab90
Remi NGUYEN VAN <reminv@google.com>
2025-01-17 17:39:07 +0900

    Fix flakes due to tests resetting CarrierConfig
    Some tests use CarrierConfigManager#overrideConfig(subId, bundle) to set
    some configs, and then overrideConfig(subId, null) to reset the
    override.  However this also resets overrides that may have been set by
    the instrumentation, such as ConnectivityTestTargetPreparer /
    CarrierConfigSetupTest.
    
    Avoid this by providing a common utility to set and tear down carrier
    config, which reset the overrides to the original setting value instead
    of clearing all overrides.
    
    Test: atest
    Bug: 253698734
    Change-Id: Ia25b28d551b62205c52f91e0ffad1b94710c0828

--- tests/cts/tethering/src/android/tethering/cts/TetheringManagerTest.java
+++ tests/cts/tethering/src/android/tethering/cts/TetheringManagerTest.java
@@ -84,9 +84,11 @@ import androidx.test.runner.AndroidJUnit4;
 
 import com.android.modules.utils.build.SdkLevel;
 import com.android.testutils.ParcelUtils;
+import com.android.testutils.com.android.testutils.CarrierConfigRule;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -102,6 +104,8 @@ import java.util.function.Consumer;
 
 @RunWith(AndroidJUnit4.class)
 public class TetheringManagerTest {
+    @Rule
+    public final CarrierConfigRule mCarrierConfigRule = new CarrierConfigRule();
 
     private Context mContext;
 
@@ -527,22 +531,13 @@ public class TetheringManagerTest {
         // Override carrier config to ignore entitlement check.
         final PersistableBundle bundle = new PersistableBundle();
         bundle.putBoolean(CarrierConfigManager.KEY_REQUIRE_ENTITLEMENT_CHECKS_BOOL, false);
-        overrideCarrierConfig(bundle);
+        mCarrierConfigRule.addConfigOverrides(
+                SubscriptionManager.getDefaultSubscriptionId(), bundle);
 
         // Verify that requestLatestTetheringEntitlementResult() can get entitlement
         // result TETHER_ERROR_NO_ERROR due to provisioning bypassed.
         assertEntitlementResult(listener -> mTM.requestLatestTetheringEntitlementResult(
                 TETHERING_WIFI, false, c -> c.run(), listener), TETHER_ERROR_NO_ERROR);
-
-        // Reset carrier config.
-        overrideCarrierConfig(null);
-    }
-
-    private void overrideCarrierConfig(PersistableBundle bundle) {
-        final CarrierConfigManager configManager = (CarrierConfigManager) mContext
-                .getSystemService(Context.CARRIER_CONFIG_SERVICE);
-        final int subId = SubscriptionManager.getDefaultSubscriptionId();
-        runAsShell(MODIFY_PHONE_STATE, () -> configManager.overrideConfig(subId, bundle));
     }
 
     private boolean isTetheringApnRequired() {
